4d1d596e08d2fba9098883fd921740acdea86324
Brandon Kase <brandon.kase@gmail.com>
2020-02-29 16:06:16 -0800

    Forgot to commit gameboy view

---
+++ src/gameboy_view.rs
@@ -0,0 +1,43 @@
+use hardware::Hardware;
+use mem::{JoypadButton, JoypadDpad, JoypadKey};
+use moxie_dom::{
+    elements::{canvas, form},
+    prelude::*,
+};
+use std::cell::RefCell;
+use std::rc::Rc;
+use std::string::String;
+
+use web_utils::log;
+
+fn button_of_key(key: &String) -> Option<JoypadKey> {
+    log(&format!("Button of key: {:}", key));
+    if key == "Enter" {
+        Some(JoypadKey::Button(JoypadButton::Start))
+    } else if key == "a" {
+        Some(JoypadKey::Button(JoypadButton::A))
+    } else {
+        None
+    }
+}
+
+#[topo::nested]
+#[illicit::from_env(hardware: &Key<Rc<RefCell<Hardware>>>)]
+pub fn gameboy_view() {
+    let hardware1 = hardware.clone();
+    let on_keydown = move |keypress: event::KeyDown| {
+        let key = button_of_key(&keypress.key());
+        key.iter()
+            .for_each(|k| hardware1.borrow_mut().cpu.memory.joypad.handle_key_down(*k));
+    };
+    let hardware2 = hardware.clone();
+    let on_keyup = move |keypress: event::KeyUp| {
+        let key = button_of_key(&keypress.key());
+        key.iter()
+            .for_each(|k| hardware2.borrow_mut().cpu.memory.joypad.handle_key_up(*k));
+    };
+
+    mox! {
+       <canvas width="160" height="144" id="canvas" style="width: 100%;image-rendering: pixelated;" on={on_keydown} on={on_keyup} tabIndex="0"></canvas>
+    };
+}
